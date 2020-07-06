e9fc7b43c5206fa2c0607b7e4f754c7b3ec0486c
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-06 15:30:15 +0300

    Create index.js

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styled from './footer.module.scss';
+
+function Footer() {
+
+    return (
+        <footer className={styled.footer}>
+            <div className="container">
+                footer
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
