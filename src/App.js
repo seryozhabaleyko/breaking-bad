f5742c9675be6d51718dd64f9a954cb99937ecc1
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-06 15:29:58 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useFetch from 'use-http';
+import { useDebouncedCallback } from 'use-debounce';
 
-import './App.module.scss';
+import Header from './components/Header';
+import Search from './components/Search';
+import Characters from './components/Characters';
+import Footer from './components/Footer';
+import { baseUrl } from './constants';
+import Spinner from './components/Spinner';
 
 function App() {
+    const [query, setQuery] = useState('');
+    const [debouncedCallback] = useDebouncedCallback((q) => {
+        setQuery(q);
+    }, 1000);
+
+    const { loading, data = [], error } = useFetch(`${baseUrl}/characters?name=${query}`, [query]);
 
     return (
         <>
-            <header>header</header>
-            <main>main</main>
-            <footer>footer</footer>
+            <Header />
+            <Spinner />
+            <main>
+                <div className="container">
+                    <Search getQuery={(q) => debouncedCallback(q)} />
+                    <Characters isLoading={loading} characters={data} error={error} />
+                </div>
+            </main>
+            <Footer />
         </>
     );
 }
